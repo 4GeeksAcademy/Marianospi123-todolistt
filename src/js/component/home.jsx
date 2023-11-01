85a70ead424dbb9dd30479057e4f3d650d7d3a2c
marianospi123 <141750385+marianospi123@users.noreply.github.com>
2023-11-01 20:37:28 +0000

    todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState("");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleTaskTextChange = (event) => {
+    setTaskText(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (taskText) {
+      setTasks([...tasks, { text: taskText, completed: false }]);
+      setTaskText("");
+    }
+  };
+
+  const handleTaskDelete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const handleTaskToggle = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1> TODOS</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Nueva tarea"
+          value={taskText}
+          onChange={handleTaskTextChange}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              handleAddTask();
+            }
+          }}
+        />
+      </div>
+      {tasks.length === 0 ? (
+        <p>No hay tareas, añadir tareas</p>
+      ) : (
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span
+                style={{
+                  textDecoration: task.completed ? "line-through" : "none",
+                }}
+                onClick={() => handleTaskToggle(index)}
+              >
+                {task.text}
+              </span>
+              {task.completed && (
+                <button onClick={() => handleTaskDelete(index)}>X</button>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+      <p className="total-tasks">Tareas por hacer: {tasks.length}</p> {/* Contador de tareas */}
+    </div>
+  );
 };
 
-export default Home;
+export default TodoList;
